1f76b9fc6ffa4cb6c6a73dcfa03acc41743e405f
infinite-Zh <zhxuwork@163.com>
2019-04-28 14:21:14 +0800

    优化

---
+++ app/src/main/java/com/infinite/vector_drawables/StatusProgressView.java
@@ -0,0 +1,25 @@
+package com.infinite.vector_drawables;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @author kfzhangxu
+ * Created on 2019/4/28.
+ */
+public class StatusProgressView extends View {
+    public StatusProgressView(Context context) {
+        super(context);
+    }
+
+    public StatusProgressView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
